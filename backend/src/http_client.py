970ecdbb12b524f82188c51830ff9cbec7ebf7e6
InvarGervi123 <InvarGervi1234@gmail.com>
2025-07-04 21:18:52 +0300

    6

--- backend/src/http_client.py
+++ backend/src/http_client.py
@@ -1,6 +1,6 @@
 from aiohttp import ClientSession
 
-class HTTPCliecnt:
+class HTTPClient:
     def __init__(self, base_url: str, api_key: str):
         self._session = ClientSession(
             base_url=base_url,
@@ -12,7 +12,7 @@ class HTTPCliecnt:
     
     
     
-class CMCHTTPCliecnt(HTTPCliecnt):
+class CMCHTTPClient(HTTPClient):
     async def get_listings(self):
         async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
             result = await resp.json()
@@ -20,8 +20,8 @@ class CMCHTTPCliecnt(HTTPCliecnt):
         
     async def get_currency(self, currency_id: int):
         async with self._session.get(
-                    "/v2/cryptocurrency/quotes/latest",
+                "/v2/cryptocurrency/quotes/latest",
                 params={"id": currency_id}
             ) as resp:
-            result = await resp.json()
-            return result["data"][str(currency_id)]
+                result = await resp.json()
+                return result["data"][str(currency_id)]
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
+from src.config import settings
+from src.http_client import CMCHTTPClient
 
 app = FastAPI()
 
 
 cmc_client = CMCHTTPClient(
-    base_url="https://pro-api.coinmarketcap.com"
+    base_url="https://pro-api.coinmarketcap.com",
     api_key=settings.CMC_API_KEY
 )
+
 
 @app.get("/cryptocurrencies")
 async def get_cryptocurrencies():
@@ -15,5 +18,6 @@ async def get_cryptocurrencies():
     
     
 @app.get("/cryptocurrencies/{currency_id}")
-async def get_crypyocurrncy(currency_id: int):
+async def get_cryptocurrency(currency_id: int):
     return await cmc_client.get_currency(currency_id)
+
