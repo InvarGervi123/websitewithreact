77df2e0332840d34186c0011eb134526f5346442
InvarGervi123 <InvarGervi1234@gmail.com>
2025-07-04 20:13:18 +0300

    4

--- backend/src/http_client.py
+++ backend/src/http_client.py
@@ -12,5 +12,16 @@ class HTTPCliecnt:
     
     
     
-class CMCHTTPCliecnt:
-    
+class CMCHTTPCliecnt(HTTPCliecnt):
+    async def get_listings(self):
+        async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
+            result = await resp.json()
+            return result["data"]
+        
+    async def get_currency(self, currency_id: int):
+        async with self._session.get(
+                    "/v2/cryptocurrency/quotes/latest",
+                params={"id": currency_id}
+            ) as resp:
+            result = await resp.json()
+            return result["data"]
